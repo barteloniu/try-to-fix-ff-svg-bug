16b001c8aa2d1462fed0bc74fab3f758b386f2f1
barteloniu <barteloniu@gmail.com>
2018-03-16 11:16:26 +0100

    update files

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const ctx = canvas.getContext("2d")
 const img = document.querySelector("#img")
 const h5 = document.querySelector("#h5")
 
-ctx.fillRect(20, 20, 40, 10)
-ctx.drawImage(img, 40, 60, 100, 100)
+let x = 0
+
+const loop = () => {
+    ctx.clearRect(0, 0, 150, 150)
+    ctx.drawImage(img, 25+Math.sin(x) * 50, 60, 100, 100)
+    x+= 0.1
+    requestAnimationFrame(loop)
+}
+requestAnimationFrame(loop)
